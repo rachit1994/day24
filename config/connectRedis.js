e5ef8f87a9b574dc38c976e30ccfc05b23df86f1
rachit1994 <rachit.srivastava1994@gmail.com>
2023-01-17 17:10:44 +0530

    reading redis from env

--- config/connectRedis.js
+++ config/connectRedis.js
@@ -1,6 +1,9 @@
 const Redis = require("redis");
+require("dotenv").config();
 
-const RedisClient = Redis.createClient();
+const RedisClient = Redis.createClient({
+  url: process.env.REDIS_URL
+});
 
 RedisClient.on('error', (err) => console.log('Redis Client Error', err));
 
